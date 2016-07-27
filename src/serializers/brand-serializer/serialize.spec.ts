9384343007d1aa6f3b3514f623d93b9ffd262e3a
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-07-27 18:29:22 +0100

    Install poseidon-builders

--- src/serializers/brand-serializer/serialize.spec.ts
+++ src/serializers/brand-serializer/serialize.spec.ts
@@ -3,10 +3,16 @@
 import { Expect, Test } from 'alsatian';
 const Poseidon = require('noodlecrate-poseidon'); // todo: fix this with poseidon typings
 
+const BrandModel = Poseidon.Models.BrandModel;
+const BrandDto = Poseidon.DTOs.BrandDto;
+const BrandSerializer = Poseidon.Serializers.BrandSerializer;
+
 export class BrandSerializerSerializeTestFixture {
 
-    @Test()
-    public exampleTest() {
+    @TestCase("Pot Noodle")
+    @TestCase("Chilli Noodle")
+    @TestCase("JavaScript Noodles")
+    public serializesNameFromModelToDto(brandName: string) {
         Expect(1 + 1).toBe(2);
     }
 
