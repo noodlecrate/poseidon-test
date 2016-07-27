16406135a5cb2446134904792d9e77baaacd0f90
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-07-27 18:09:00 +0100

    Initial setup

--- src/serializers/brand-serializer/serialize.spec.ts
+++ src/serializers/brand-serializer/serialize.spec.ts
@@ -1,1 +1,13 @@
+/// <reference path="../../../typings/index.d.ts" />
+
 import { Expect, Test } from 'alsatian';
+const Poseidon = require('noodlecrate-poseidon'); // todo: fix this with poseidon typings
+
+export class BrandSerializerSerializeTestFixture {
+
+    @Test()
+    public exampleTest() {
+        Expect(1 + 1).toBe(2);
+    }
+
+}
